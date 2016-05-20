b88bc05ef0b748d26c6275a92e39807eaa98e02b
Lucas REYNES <lucasreynes@hotmail.fr>
2016-05-20 15:28:10 +0200

    Données de test graphique CA

--- Gestion-dun-pressing/Dashboard.Designer.cs
+++ Gestion-dun-pressing/Dashboard.Designer.cs
@@ -28,14 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea57 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series57 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title57 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea58 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend29 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series58 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title58 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.questionLbl = new System.Windows.Forms.Label();
             this.statLbl = new System.Windows.Forms.Label();
             this.gererClientBtn = new System.Windows.Forms.Button();
@@ -54,7 +53,7 @@
             // 
             this.questionLbl.AutoSize = true;
             this.questionLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.questionLbl.Location = new System.Drawing.Point(472, 35);
+            this.questionLbl.Location = new System.Drawing.Point(658, 48);
             this.questionLbl.Name = "questionLbl";
             this.questionLbl.Size = new System.Drawing.Size(360, 37);
             this.questionLbl.TabIndex = 0;
@@ -62,9 +61,8 @@
             // 
             // statLbl
             // 
-            this.statLbl.AutoSize = true;
             this.statLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statLbl.Location = new System.Drawing.Point(554, 342);
+            this.statLbl.Location = new System.Drawing.Point(744, 406);
             this.statLbl.Name = "statLbl";
             this.statLbl.Size = new System.Drawing.Size(182, 37);
             this.statLbl.TabIndex = 1;
@@ -73,7 +71,7 @@
             // gererClientBtn
             // 
             this.gererClientBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.gererClientBtn.Location = new System.Drawing.Point(410, 114);
+            this.gererClientBtn.Location = new System.Drawing.Point(596, 127);
             this.gererClientBtn.Name = "gererClientBtn";
             this.gererClientBtn.Size = new System.Drawing.Size(225, 54);
             this.gererClientBtn.TabIndex = 2;
@@ -83,7 +81,7 @@
             // gererProduitBtn
             // 
             this.gererProduitBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.gererProduitBtn.Location = new System.Drawing.Point(669, 114);
+            this.gererProduitBtn.Location = new System.Drawing.Point(855, 127);
             this.gererProduitBtn.Name = "gererProduitBtn";
             this.gererProduitBtn.Size = new System.Drawing.Size(225, 54);
             this.gererProduitBtn.TabIndex = 3;
@@ -93,7 +91,7 @@
             // gererEmployeBtn
             // 
             this.gererEmployeBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.gererEmployeBtn.Location = new System.Drawing.Point(410, 196);
+            this.gererEmployeBtn.Location = new System.Drawing.Point(596, 209);
             this.gererEmployeBtn.Name = "gererEmployeBtn";
             this.gererEmployeBtn.Size = new System.Drawing.Size(225, 54);
             this.gererEmployeBtn.TabIndex = 4;
@@ -103,7 +101,7 @@
             // gererPrestationBtn
             // 
             this.gererPrestationBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.gererPrestationBtn.Location = new System.Drawing.Point(669, 196);
+            this.gererPrestationBtn.Location = new System.Drawing.Point(855, 209);
             this.gererPrestationBtn.Name = "gererPrestationBtn";
             this.gererPrestationBtn.Size = new System.Drawing.Size(225, 54);
             this.gererPrestationBtn.TabIndex = 5;
@@ -112,49 +110,47 @@
             // 
             // CAChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.CAChart.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.CAChart.Legends.Add(legend1);
-            this.CAChart.Location = new System.Drawing.Point(12, 409);
+            chartArea57.Name = "ChartArea1";
+            this.CAChart.ChartAreas.Add(chartArea57);
+            this.CAChart.Location = new System.Drawing.Point(27, 505);
             this.CAChart.Name = "CAChart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.CAChart.Series.Add(series1);
-            this.CAChart.Size = new System.Drawing.Size(771, 428);
+            series57.ChartArea = "ChartArea1";
+            series57.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series57.Name = "ChiffreAffaire";
+            this.CAChart.Series.Add(series57);
+            this.CAChart.Size = new System.Drawing.Size(1021, 531);
             this.CAChart.TabIndex = 6;
             this.CAChart.Text = "Chiffre d\'affaire";
-            title1.Name = "Vide pour le titre en label";
-            this.CAChart.Titles.Add(title1);
+            title57.Name = "Vide pour le titre en label";
+            this.CAChart.Titles.Add(title57);
             // 
             // typesProduitsChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.typesProduitsChart.ChartAreas.Add(chartArea2);
-            legend2.Alignment = System.Drawing.StringAlignment.Center;
-            legend2.Name = "Legend1";
-            this.typesProduitsChart.Legends.Add(legend2);
-            this.typesProduitsChart.Location = new System.Drawing.Point(812, 409);
+            chartArea58.Name = "ChartArea1";
+            this.typesProduitsChart.ChartAreas.Add(chartArea58);
+            legend29.Alignment = System.Drawing.StringAlignment.Center;
+            legend29.Name = "Legend1";
+            this.typesProduitsChart.Legends.Add(legend29);
+            this.typesProduitsChart.Location = new System.Drawing.Point(1083, 505);
             this.typesProduitsChart.Name = "typesProduitsChart";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            series2.YValuesPerPoint = 2;
-            this.typesProduitsChart.Series.Add(series2);
-            this.typesProduitsChart.Size = new System.Drawing.Size(430, 428);
+            series58.ChartArea = "ChartArea1";
+            series58.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series58.Legend = "Legend1";
+            series58.Name = "Series1";
+            series58.YValuesPerPoint = 2;
+            this.typesProduitsChart.Series.Add(series58);
+            this.typesProduitsChart.Size = new System.Drawing.Size(511, 531);
             this.typesProduitsChart.TabIndex = 7;
             this.typesProduitsChart.Text = "Types de produits";
-            title2.Name = "Title1";
-            title2.Text = "Types de produits";
-            this.typesProduitsChart.Titles.Add(title2);
+            title58.Name = "Title1";
+            title58.Text = "Types de produits";
+            this.typesProduitsChart.Titles.Add(title58);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(264, 421);
+            this.label1.Location = new System.Drawing.Point(393, 521);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(168, 25);
             this.label1.TabIndex = 8;
@@ -165,7 +161,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.ForeColor = System.Drawing.Color.Silver;
-            this.label2.Location = new System.Drawing.Point(427, 421);
+            this.label2.Location = new System.Drawing.Point(556, 521);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(153, 25);
             this.label2.TabIndex = 9;
@@ -175,7 +171,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1254, 849);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1624, 1079);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.typesProduitsChart);
@@ -187,11 +184,13 @@
             this.Controls.Add(this.statLbl);
             this.Controls.Add(this.questionLbl);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1280, 920);
-            this.MinimumSize = new System.Drawing.Size(1280, 920);
+            this.MaximumSize = new System.Drawing.Size(1650, 1150);
+            this.MinimumSize = new System.Drawing.Size(1650, 1150);
             this.Name = "Dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Dashboard_Load);
             ((System.ComponentModel.ISupportInitialize)(this.CAChart)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.typesProduitsChart)).EndInit();
             this.ResumeLayout(false);
--- Gestion-dun-pressing/Dashboard.cs
+++ Gestion-dun-pressing/Dashboard.cs
@@ -16,5 +16,21 @@ namespace Gestion_dun_pressing
         {
             InitializeComponent();
         }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Jan", "10 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Fév", "5 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Mars", "3 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Avr", "23 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Mai", "1 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Juin", "15 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Juil", "13 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Août", "12 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Sept", "10 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Oct", "13 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Nov", "1 000");
+            CAChart.Series["ChiffreAffaire"].Points.AddXY("Déc", "20 000");
+        }
     }
 }
