3f18b5b8a6c84eaa405535686f2b064693b9f04e
Lucas REYNES <lucasreynes@hotmail.fr>
2016-05-24 14:03:34 +0200

    Ajout classe Client

---
+++ Gestion-dun-pressing/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_dun_pressing
+{
+    public static class Client
+    {
+
+    }
+}
