df14099c39f07a1da082f74149acdbc2e1abafde
Lucas REYNES <lucasreynes@hotmail.fr>
2016-05-24 16:15:56 +0200

    Ajout de la classe Prestation

---
+++ Gestion-dun-pressing/Prestation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Gestion_dun_pressing
+{
+    public static class Prestation
+    {
+        public static DataTable prestations()
+        {
+            return GestionBDD.select("prestations");
+        }
+    }
+}
