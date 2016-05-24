1d9282a2b30091dabe38c65ddf0de1c9c9a4039f
Lucas REYNES <lucasreynes@hotmail.fr>
2016-05-24 16:04:16 +0200

    Modification classe Produit

--- Gestion-dun-pressing/Produit.cs
+++ Gestion-dun-pressing/Produit.cs
@@ -11,19 +11,19 @@ namespace Gestion_dun_pressing
     {
         public static DataTable produits()
         {
-            return GestionBDD.select("Produit");
+            return GestionBDD.select("produits");
         }
 
         public static DataTable modifier(int id, string type, DateTime dateCreation)
         {
-            string requete = string.Format("UPDATE Produit SET id={0}, type='{1}', created_at='{2}' WHERE id={0}", id, type, dateCreation);
+            string requete = string.Format("UPDATE produits SET id={0}, type='{1}', created_at='{2}' WHERE id={0}", id, type, dateCreation);
             GestionBDD.editById(requete);
             return produits();
         }
 
         public static DataTable supprimer(int id)
         {
-            GestionBDD.deleteById(id, "Produit");
+            GestionBDD.deleteById(id, "produits");
             return produits();
         }
     }
